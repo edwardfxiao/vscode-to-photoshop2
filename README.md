5ddf8bc329427d904b078a0db5c6672a4810484f
Edward Xiao <edwardfxiao@gmail.com>
2024-10-22 12:07:45 +0800

    - init

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# vscode-to-photoshop2
+# vscode-to-photoshop2 README
+
+This extension is a fork of [vscode-to-photoshop](https://github.com/bialikover/vscode-to-photoshop)
+
+
+### [vscode-to-photoshop](https://github.com/bialikover/vscode-to-photoshop):
+
+```js
+// my-phoroshop-javascript.js on macOS
+
+var currentScriptFile = new File($.fileName);
+alert(currentScriptFile.toString());
+
+// ~/.vscode/extensions/bialikover.vscode-to-photoshop-0.0.3/out/src/tmp_file.jsx
+```
+
+### [vscode-to-photoshop2](https://github.com/edwardfxiao/vscode-to-photoshop2):
+```js
+// my-phoroshop-javascript.js on macOS
+
+var JSON = {
+	 parse: function (str) {
+	    return eval('(' + str + ')');
+	  }
+	};
+}
+var actualFilename = "";
+try {
+  var option = JSON.parse(arguments[0]);
+  actualFilename = option.filename;
+}
+catch(e){}
+alert(actualFilename);
+// ~/my-actual-file-path/my-actual-file.jsx
+```
+
+
+
+What this new extension does is to provide a actual file path as [this issue states](https://github.com/bialikover/vscode-to-photoshop/issues/3) states.
---
+++ src/extension.ts
@@ -0,0 +1,60 @@
+'use strict';
+
+import * as vscode from 'vscode';
+/* @ts-ignore */
+import * as osascript from 'node-osascript';
+import * as fs from 'fs';
+import * as cs from 'child_process';
+import * as path from 'path';
+
+const WINDOWS = 'win32';
+const OSX = 'darwin';
+
+export function activate(context: vscode.ExtensionContext) {
+
+  console.log('vscode-to-photoshop2 is now active');
+  let outputChannel = vscode.window.createOutputChannel('Extend Script');
+  let disposable = vscode.commands.registerCommand('extension.evaluateScript2', () => {
+    vscode.window.showInformationMessage('Evaluating Extend Script');
+
+    let editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return;
+    }
+    let selection = editor.selection;
+    let text = selection.isEmpty ? editor.document.getText() : editor.document.getText(selection);
+
+    let pathTojsx = path.resolve(__dirname, 'tmp_file.jsx');
+    fs.writeFile(pathTojsx, text, (err) => {
+      if (err) { return showOutput(`error: ${err.message} \n stack: ${err.stack}`); }
+      //show the output.
+      outputChannel.show();
+
+      if (process.platform === WINDOWS) {
+        let script = `wscript ${path.resolve(__dirname, '../../photoshop.vbs')} ${pathTojsx} "${JSON.stringify({ filename: `'${editor.document.fileName}'` })}"`;
+        cs.exec(script, cb);
+      }
+
+      if (process.platform === OSX) {
+        let script = `tell application id "com.adobe.Photoshop" to do javascript ("#include ${pathTojsx}") with arguments "{filename: '${editor.document.fileName}'}"`;
+        osascript.execute(script, cb);
+      }
+
+    });
+
+    function cb(err: any, res: any, raw: any) {
+      if (err) { return showOutput(`error: ${err.message} \n stack: ${err.stack}`); };
+      showOutput(res);
+    }
+
+    function showOutput(message: string) {
+      outputChannel.append(`${new Date().toISOString()} ${message}`);
+    }
+  });
+
+  context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
